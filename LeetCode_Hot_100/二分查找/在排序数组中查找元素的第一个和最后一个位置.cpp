bdfa66003abfd3f9c74cc9db644709f654af95a4
wang-qin928 <2830862261@qq.com>
2025-10-28 20:55:15 +0800

    update hot100 在排序数组中查找元素的第一个和最后一个位置 二分后扩展左右边界

---
+++ LeetCode_Hot_100/二分查找/在排序数组中查找元素的第一个和最后一个位置.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int begin = -1, end = -1;
+
+        // 查找右边界
+        int left = 0, right = nums.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (target == nums[mid]) {
+                // 找到 mid 后往两边寻找边界
+                begin = mid, end = mid;
+                // 此时左边界下标必须大于前一次二分的左边界下标
+                while (begin > left && target == nums[begin - 1])
+                    begin--; // 如果左边的位置满足两个条件 再减减
+                // 同样 此时右边界下标必须小于前一次二分的右边界下标
+                while (end < right && target == nums[end + 1])
+                    end++;
+                break; // 已经找到左右边界 无需再次二分
+            } else if (target > nums[mid])
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        return {begin, end};
+    }
+};
+
+// link : https://leetcode.cn/problems/find-first-and-last-position-of-element-in-sorted-array/description/?envType=study-plan-v2&envId=top-100-liked
