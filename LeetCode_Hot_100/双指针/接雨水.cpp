037495288a367867bc470be94520447e76c4efb1
wang-qin928 <2830862261@qq.com>
2025-11-15 19:54:06 +0800

    update 第二遍 hot100 接雨水 双指针

--- LeetCode_Hot_100/双指针/接雨水.cpp
+++ LeetCode_Hot_100/双指针/接雨水.cpp
@@ -34,3 +34,31 @@ public:
 };
 
 // link : https://leetcode.cn/problems/trapping-rain-water/?envType=study-plan-v2&envId=top-100-liked
+
+// 第二次写:
+class Solution {
+public:
+    int trap(vector<int>& h) {
+        int n = h.size();
+        int sum = 0, max_layer = 0;
+        for (auto e : h) {
+            sum += e;                                    // 计算整个黑色块的和
+            max_layer = (e > max_layer) ? e : max_layer; // 计算最大层数
+        }
+        int left = 0, right = n - 1, layer = 1; // 当前所处层数
+
+        int summary = 0;
+        while (left <= right && layer <= max_layer) {
+            while (h[left] < layer)
+                left++;
+            while (h[right] < layer)
+                right--;
+            summary += (right - left + 1); // 加上当前层数黑色块和水的总量
+            layer++;
+            // cout << summary << " " << layer << endl;
+        }
+        // cout << summary << " " << layer << " " << sum << " " << max_layer
+        //      << endl;
+        return summary - sum; // 用总和减去只有黑色块的数量
+    }
+};
