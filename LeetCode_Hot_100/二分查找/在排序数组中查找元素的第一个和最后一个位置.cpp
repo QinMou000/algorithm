82a7569d3bff5e8269f36d70fad968ab5d382c80
wang-qin928 <2830862261@qq.com>
2025-11-20 22:06:15 +0800

    update hot100在排序数组中查找元素的第一个和最后一个位置 第二遍 二分 注意边界问题

--- LeetCode_Hot_100/二分查找/在排序数组中查找元素的第一个和最后一个位置.cpp
+++ LeetCode_Hot_100/二分查找/在排序数组中查找元素的第一个和最后一个位置.cpp
@@ -27,3 +27,32 @@ public:
 };
 
 // link : https://leetcode.cn/problems/find-first-and-last-position-of-element-in-sorted-array/description/?envType=study-plan-v2&envId=top-100-liked
+
+// 第二遍写
+class Solution {
+public:
+    int lower_bound(vector<int>& nums, int target) {
+        int n = nums.size();
+        int left = 0, right = n - 1;
+        while (left <= right) {
+            int mid = left + ((right - left) >> 1);
+            if (nums[mid] < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        return left; // 这样求出来的是左边界
+    }
+
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int n = nums.size();
+        int begin = lower_bound(nums, target); // 求左边界
+        if (nums.size() == begin || nums[begin] != target)
+            return {-1, -1};
+        int end = lower_bound(nums, target + 1) - 1; // 求右边界
+        return {begin, end};
+    }
+};
+
+// link :
+// https://leetcode.cn/problems/find-first-and-last-position-of-element-in-sorted-array/description/?envType=study-plan-v2&envId=top-100-liked
