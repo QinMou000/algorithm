e5679cac2720158ec9000827a1a44c701735a768
wang-qin928 <2830862261@qq.com>
2025-11-30 14:49:41 +0800

    update 剑指offer 找出字符串中第一个匹配项的下标 两层for

--- LeetCode面试经典150题/H指数.cpp
+++ LeetCode面试经典150题/H指数.cpp
@@ -38,27 +38,48 @@
 //     }
 // };
 
-// 时间 O(nlogn)
+// // 时间 O(nlogn)
+// // 空间 O(1)
+// class Solution {
+// public:
+//     int hIndex(vector<int>& arr) {
+//         // 二分
+//         // 结果最大就是数组大小 即发表论文的篇数
+//         int left = 0, right = arr.size();
+//         while (left <= right) {
+//             int mid = left + (right - left) / 2;
+//             int cnt = 0;
+//             for (int i = 0; i < arr.size(); i++)
+//                 if (arr[i] >= mid)
+//                     cnt++;
+//             if (mid <= cnt) // 满足条件尝试更大的结果
+//                 left = mid + 1;
+//             else
+//                 right = mid - 1;
+//         }
+//         return right; // 返回最后一个满足条件的值也就是right
+//     }
+// };
+
+// 时间 O(n)
 // 空间 O(1)
 class Solution {
 public:
     int hIndex(vector<int>& arr) {
-        // 二分
-        // 结果最大就是数组大小 即发表论文的篇数
-        int left = 0, right = arr.size();
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            int cnt = 0;
-            for (int i = 0; i < arr.size(); i++)
-                if (arr[i] >= mid)
-                    cnt++;
-            if (mid <= cnt) // 满足条件尝试更大的结果
-                left = mid + 1;
-            else
-                right = mid - 1;
+        // 计数
+        int n = arr.size();
+        vector<int> cnt(n + 1);
+        for (auto& e : arr)
+            cnt[min(n, e)]++; // 统计计数
+        int sum = 0;
+        for (int i = n;; i--) {
+            sum += cnt[i]; // 将大于等于 i 次计数的论文数累加
+            if (sum >= i)
+                return i;
         }
-        return right; // 返回最后一个满足条件的值也就是right
     }
 };
 
 // link : https://leetcode.cn/problems/h-index/description/?envType=study-plan-v2&envId=top-interview-150
+
+// 豆包老师题解 : https://www.doubao.com/chat/31824991281264642
---
+++ 剑指offer/找出字符串中第一个匹配项的下标.cpp
@@ -0,0 +1,45 @@
+// class Solution {
+// public:
+//     //  第一次写 这个代码好丑陋
+//     int strStr(string str, string s) {
+//         int n = str.size();
+//         int m = s.size();
+//         if (n < m)
+//             return -1;
+//         for (int i = 0; i < n; i++) {
+//             if (str[i] == s[0]) {
+//                 int index = 0;    // 给s用的下标
+//                 bool flag = true; // 先假设这个i就是答案
+//                 for (int j = i; j < n && index < m; j++) {
+//                     cout << str[j] << " " << s[index] << endl;
+//                     if (str[j] != s[index++]) {
+//                         flag = false;
+//                     }
+//                 }
+//                 if (index == m && flag)
+//                     return i;
+//             }
+//         }
+//         return -1;
+//     }
+// };
+
+class Solution {
+public:
+    int strStr(string str, string s) {
+        int n = str.size(), m = s.size();
+        for (int i = 0; i <= n - m; i++) {
+            int j = i, k = 0; // j是str使用的下标 k是s使用的下标
+            while (k < m && str[j] == s[k]) {
+                j++;
+                k++;
+            }
+            if (k == m)
+                return i;
+        }
+        return -1;
+    }
+};
+
+
+// link : https://leetcode.cn/problems/find-the-index-of-the-first-occurrence-in-a-string/description/?envType=study-plan-v2&envId=top-interview-150
