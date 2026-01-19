9b7c6b4dadbcbf47f27b56072c01ae0d388a4740
wang-qin928 <2830862261@qq.com>
2026-01-19 17:17:34 +0800

    update 排序链表 归并

--- LeetCode_Hot_100/链表/排序链表.cpp
+++ LeetCode_Hot_100/链表/排序链表.cpp
@@ -9,7 +9,7 @@
  * };
  */
 class Solution {
-public:
+  public:
     ListNode* MergeList(ListNode* left, ListNode* right) {
         ListNode* tmp = new ListNode(-1); // 哨兵头结点
         ListNode* cur = tmp;
@@ -28,8 +28,9 @@ public:
         return tmp->next; // 剔除哨兵节点
     }
     ListNode* MergeSort(ListNode* head) {
-        if (!head || !head->next) // 如果没有节点或者只有一个节点直接返回
+        if (!head || !head->next) { // 如果没有节点或者只有一个节点直接返回
             return head;
+        }
         ListNode *fast = head->next /*fast要快一步*/, *slow = head;
         while (fast && fast->next) { // 求出中间节点
             fast = fast->next->next;
@@ -41,9 +42,54 @@ public:
         ListNode* l = MergeSort(head); // 对左边归并
         return MergeList(r, l);        // 将两边的已经排好序的节点归并
     }
-    ListNode* sortList(ListNode* head) { return MergeSort(head); }
+    ListNode* sortList(ListNode* head) {
+        return MergeSort(head);
+    }
 };
 
 // 快排方式 : TODO
 
+
+// 第二遍写：
+class Solution {
+  public:
+    ListNode* sortList(ListNode* head) {
+        return MergeList(head);
+    }
+    ListNode* MergeList(ListNode* head) {
+        if (!head || !head->next) {
+            return head;    // 如果没有节点或者只有一个节点直接返回自身
+        }
+        // 寻找中间节点
+        ListNode* fast = head->next;
+        ListNode* slow = head;
+        while (fast && fast->next) {
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+
+        ListNode* right = MergeList(slow->next); // 对右边归并
+        slow->next = nullptr; // 设定左边归并的终点 最后一次递归会将两部分连接
+        ListNode* left = MergeList(head); // 对左边归并
+
+        return MergeList(left, right); // 将两边的已经排好序的节点归并
+    }
+
+    ListNode* MergeList(ListNode* p1, ListNode* p2) {
+        ListNode* tmp = new ListNode(-1); // 哨兵节点
+        ListNode* cur = tmp;
+        while (p1 && p2) {
+            if (p1->val > p2->val) {
+                cur->next = p2;
+                p2 = p2->next;
+            } else {
+                cur->next = p1;
+                p1 = p1->next;
+            }
+            cur = cur->next;
+        }
+        cur->next = p1 ? p1 : p2;
+        return tmp->next; // 剔除哨兵节点
+    }
+};
 // link : https://leetcode.cn/problems/sort-list/description/?envType=study-plan-v2&envId=top-100-liked
