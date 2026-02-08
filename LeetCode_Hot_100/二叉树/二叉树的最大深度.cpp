71999933da10a0054e7fba924a2f3f62ab78018a
wang-qin928 <2830862261@qq.com>
2026-02-08 20:11:26 +0800

    update 二叉树的最大深度 广度优先

--- LeetCode_Hot_100/二叉树/二叉树的最大深度.cpp
+++ LeetCode_Hot_100/二叉树/二叉树的最大深度.cpp
@@ -20,3 +20,33 @@ public:
 };
 
 // link : https://leetcode.cn/problems/maximum-depth-of-binary-tree/?envType=study-plan-v2&envId=top-100-liked
+
+
+// 广度优先搜索
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (!root)
+            return 0;
+        queue<TreeNode*> q;
+        q.push(root);
+        int ans = 0;
+        // 层序遍历
+        while (!q.empty()) {
+            int size = q.size();
+            // 遍历这一层所有节点
+            // 并把下一层节点添加到队列
+            while (size) {
+                TreeNode* cur = q.front();
+                q.pop();
+                if (cur->left)
+                    q.push(cur->left);
+                if (cur->right)
+                    q.push(cur->right);
+                size--;
+            }
+            ans++;
+        }
+        return ans;
+    }
+};
---
+++ LeetCode_Hot_100/回溯/分割回文串.cpp
@@ -0,0 +1,11 @@
+
+
+
+
+
+
+
+
+
+// TODO https://leetcode.cn/problems/palindrome-partitioning/description/?envType=study-plan-v2&envId=top-100-liked
+
