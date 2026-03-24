383c8779d13d346c7aacc3b0b72cf94ff8098e65
wang-qin928 <2830862261@qq.com>
2026-03-24 22:42:10 +0800

    update hot100 盛水最多的容器 双指针

--- LeetCode_Hot_100/双指针/盛最多水的容器.cpp
+++ LeetCode_Hot_100/双指针/盛最多水的容器.cpp
@@ -20,3 +20,23 @@ public:
         return ans;
     }
 };
+// 第二遍写
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int n = height.size();
+        int left = 0, right = n - 1;
+        int ans = 0;
+        while (left < right) {
+            int sum = min(height[left], height[right]) * (right - left);
+            ans = sum > ans ? sum : ans;
+            if (height[left] > height[right])
+                right--;
+            else
+                left++;
+        }
+        return ans;
+    }
+};
+
+// link : https://leetcode.cn/problems/container-with-most-water/?envType=study-plan-v2&envId=top-100-liked
