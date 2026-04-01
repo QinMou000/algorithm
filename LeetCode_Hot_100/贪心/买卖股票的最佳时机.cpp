0bf510c72ad51c358d9827be96818b93a594c7b8
wang-qin928 <2830862261@qq.com>
2026-04-01 16:16:15 +0800

    feat 买卖股票的最佳时机 贪心

--- LeetCode_Hot_100/贪心/买卖股票的最佳时机.cpp
+++ LeetCode_Hot_100/贪心/买卖股票的最佳时机.cpp
@@ -42,3 +42,20 @@ public:
 // 链接：https://leetcode.cn/problems/best-time-to-buy-and-sell-stock/solutions/136684/121-mai-mai-gu-piao-de-zui-jia-shi-ji-by-leetcode-/
 // 来源：力扣（LeetCode）
 // 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+// 第二遍写
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int min_prices = INT_MAX;
+        int max_profit = 0;
+        for (int i = 0; i < n; i++) {
+            if (prices[i] < min_prices)
+                min_prices = prices[i];
+            if (max_profit < prices[i] - min_prices)
+                max_profit = prices[i] - min_prices;
+        }
+        return max_profit;
+    }
+};
