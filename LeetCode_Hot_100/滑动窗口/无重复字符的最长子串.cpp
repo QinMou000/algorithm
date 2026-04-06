3d98bf128150ce4157bccd0facf04522403b1e08
wang-qin928 <2830862261@qq.com>
2026-04-06 18:53:50 +0800

    feat 无重复字符的最长子串

--- LeetCode_Hot_100/滑动窗口/无重复字符的最长子串.cpp
+++ LeetCode_Hot_100/滑动窗口/无重复字符的最长子串.cpp
@@ -19,4 +19,24 @@ public:
     }
 };
 
+// 第二遍写
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_map<char, int> map;
+        int ans = 0;
+        for (int left = 0, right = 0; right < s.size(); right++) {
+            // 入窗口
+            map[s[right]]++;
+            // 判断
+            while (map[s[right]] > 1) {
+                map[s[left++]]--;
+            }
+            ans = max(ans, right - left + 1);
+            // 出窗口
+        }
+        return ans;
+    }
+};
+
 // link : https://leetcode.cn/problems/longest-substring-without-repeating-characters/description/?envType=study-plan-v2&envId=top-100-liked
