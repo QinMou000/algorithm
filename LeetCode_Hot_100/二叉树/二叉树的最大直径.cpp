b071a7f3a919cbf652f4864ca237b9e73c17b5ec
wang-qin928 <2830862261@qq.com>
2026-04-12 20:03:17 +0800

    二叉树的最大直径 dfs记录左右子树的最大深度

--- LeetCode_Hot_100/二叉树/二叉树的最大直径.cpp
+++ LeetCode_Hot_100/二叉树/二叉树的最大直径.cpp
@@ -28,4 +28,23 @@ public:
     }
 };
 
+// 第二遍写
+class Solution {
+public:
+    int ans = 0;
+    int diameterOfBinaryTree(TreeNode* root) {
+        dfs(root);
+        return ans;
+    }
+    // 找左右子树的最大深度
+    int dfs(TreeNode* root) {
+        if (!root)
+            return 0;
+        int left_len = dfs(root->left);
+        int right_len = dfs(root->right);
+        ans = max(left_len + right_len, ans);
+        return max(left_len, right_len) + 1;
+    }
+};
+
 // link : https://leetcode.cn/problems/diameter-of-binary-tree/description/?envType=study-plan-v2&envId=top-100-liked
