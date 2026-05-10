8d1e5c7ad36cbf2d06bcafbbeaf2bff1763e61a4
wang-qin928 <2830862261@qq.com>
2026-05-10 18:50:03 +0800

    feat 字母异位词分组  排序+哈希

--- 哈希/字母异位词分组.cpp
+++ 哈希/字母异位词分组.cpp
@@ -16,4 +16,25 @@ public:
     }
 };
 
+// 第二遍写
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> ans;
+        unordered_map<string, vector<string>> map;
+        for (auto str : strs) {
+            string tmp = str;
+            sort(str.begin(), str.end());
+            if (map.find(str) == map.end()) {
+                map[str];
+            }
+            map[str].push_back(tmp);
+        }
+        for (auto arr : map) {
+            ans.push_back(arr.second);
+        }
+        return ans;
+    }
+};
+
 // link : https://leetcode.cn/problems/group-anagrams/description/?envType=problem-list-v2&envId=hash-table
