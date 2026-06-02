f623aa86444b1141edc3a2b25ab9c0a0a02a12ed
wang-qin928 <2830862261@qq.com>
2026-06-02 21:00:47 +0800

    feat 二叉树中第k小的元素 中序递归or栈迭代

--- LeetCode_Hot_100/二叉树/二叉树中第k小的元素.cpp
+++ LeetCode_Hot_100/二叉树/二叉树中第k小的元素.cpp
@@ -46,4 +46,70 @@ public: // 和中序遍历几乎一样
     }
 };
 
+// 第二遍写:
+
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+//  * right(right) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     // 中序递归
+//     int ans = 0;
+//     int kthSmallest(TreeNode* root, int k) {
+//         int cur = 0;
+//         dfs(root, k, cur);
+//         return ans;
+//     }
+//     // cur需要传引用每次都对同一个副本进行++
+//     void dfs(TreeNode* root, int k, int& cur) {
+//         if (!root)
+//             return;
+//         dfs(root->left, k, cur);
+//         cur++;
+//         if (cur == k) {
+//             ans = root->val;
+//             return;
+//         }
+//         dfs(root->right, k, cur);
+//     }
+// };
+
+class Solution {
+public:
+    // 用栈来迭代
+    int kthSmallest(TreeNode* root, int k) {
+        stack<TreeNode*> stk;
+        TreeNode* cur = root;
+        int cnt = 0;
+
+        while (cur || !stk.empty()) {
+            while (cur) {
+                // 一直向左走
+                stk.push(cur);
+                cur = cur->left;
+            }
+            // 弹出最左边的节点
+            cur = stk.top();
+            stk.pop();
+            // 访问当前节点
+            cnt++;
+            if (cnt == k)
+                return cur->val;
+
+            // 访问当前节点的右子树
+            cur = cur->right;
+        }
+        return -1;
+    }
+};
+
 // link : https://leetcode.cn/problems/kth-smallest-element-in-a-bst/description/?envType=study-plan-v2&envId=top-100-liked
