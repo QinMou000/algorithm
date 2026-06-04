a2a989924d1efbd1886572120c8bc4fbc52c9f37
wang-qin928 <2830862261@qq.com>
2026-06-04 19:57:45 +0800

    feat 二叉树中的最大路径和 dfs 分别搜索左右子树的最大路径

--- LeetCode_Hot_100/二叉树/二叉树中的最大路径和.cpp
+++ LeetCode_Hot_100/二叉树/二叉树中的最大路径和.cpp
@@ -32,4 +32,38 @@ public:
     }
 };
 
+// 第二遍写：
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int ans = INT_MIN;
+    int maxPathSum(TreeNode* root) {
+        dfs(root);
+        return ans;
+    }
+    // 返回当前节点单边最大路径
+    int dfs(TreeNode* cur) {
+        if (!cur)
+            return 0;
+        // 舍弃单边路径小于零的部分
+        int left = max(0, dfs(cur->left));
+        int right = max(0, dfs(cur->right));
+        // 更新结果
+        ans = max(ans, left + right + cur->val);
+        // 返回给上一层
+        return max(left, right) + cur->val;
+    }
+};
+
 // link : https://leetcode.cn/problems/binary-tree-maximum-path-sum/description/?envType=study-plan-v2&envId=top-100-liked
