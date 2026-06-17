8bdfc3d6aaf9157518a2b678a770e57ae05c5063
wang-qin928 <2830862261@qq.com>
2026-06-17 19:30:09 +0800

    feat 随记链表的复制 map记录

--- LeetCode_Hot_100/链表/随机链表的复制.cpp
+++ LeetCode_Hot_100/链表/随机链表的复制.cpp
@@ -37,4 +37,28 @@ public:
     }
 };
 
+// 第二遍写：
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        if (!head)
+            return nullptr;
+        Node* cur = head;
+        unordered_map<Node*, Node*> map;
+        // 复制各节点，并建立 “原节点 -> 新节点” 的 Map 映射
+        while (cur) {
+            map[cur] = new Node(cur->val);
+            cur = cur->next;
+        }
+        cur = head;
+        // 构建新链表的 next 和 random 指向
+        while (cur) {
+            map[cur]->next = map[cur->next];
+            map[cur]->random = map[cur->random];
+            cur = cur->next;
+        }
+        // 返回新链表的头节点
+        return map[head];
+    }
+};
 // link : https://leetcode.cn/problems/copy-list-with-random-pointer/?envType=study-plan-v2&envId=top-100-liked
