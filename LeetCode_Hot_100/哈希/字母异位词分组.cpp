1770be30b76a70210e318a1b03853b8b13827865
wang-qin928 <2830862261@qq.com>
2026-06-26 10:44:44 +0800

    feat 字母异位词分组 hash + 排序

--- LeetCode_Hot_100/哈希/字母异位词分组.cpp
+++ LeetCode_Hot_100/哈希/字母异位词分组.cpp
@@ -15,4 +15,22 @@ public:
         return ans;
     }
 };
+
+// 第二遍写：
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> map; // 排序后 -> 排序前
+        for (auto& s : strs) {
+            string tmp = s;
+            sort(tmp.begin(), tmp.end());
+            map[tmp].emplace_back(s); // 排序之后字典序相同的串就自动放在一起了
+        }
+        vector<vector<string>> ans;
+        for (auto& it : map)
+            ans.emplace_back(it.second);
+        return ans;
+    }
+};
+
 // link : https://leetcode.cn/problems/group-anagrams/description/?envType=study-plan-v2&envId=top-100-liked
