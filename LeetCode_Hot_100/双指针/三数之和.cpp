7cb7ac07d9c359aea3a7b16747fec207134f8aea
wang-qin928 <2830862261@qq.com>
2026-06-29 13:14:32 +0800

    feat 三数之和 排序+双指针+去重

--- LeetCode_Hot_100/双指针/三数之和.cpp
+++ LeetCode_Hot_100/双指针/三数之和.cpp
@@ -44,4 +44,35 @@ public:
     }
 };
 
+// 第二遍写:
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < n - 1; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) // 去重
+                continue;
+            int left = i + 1, right = n - 1; // 左闭右闭
+            while (left < right) {
+                int tmp = nums[left] + nums[right] + nums[i];
+                if (tmp > 0)
+                    right--;
+                else if (tmp < 0)
+                    left++;
+                else {
+                    ans.push_back({nums[i], nums[left++], nums[right--]});
+                    // 防止重复的三元组出现
+                    while (left < right && nums[left] == nums[left - 1])
+                        left++;
+                    while (left < right && nums[right] == nums[right + 1])
+                        right--;
+                }
+            }
+        }
+        return ans;
+    }
+};
+
 // link : https://leetcode.cn/problems/3sum/description/?envType=study-plan-v2&envId=top-100-liked
