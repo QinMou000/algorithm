d3940c96a1d6a57facfbcb6b90ad9e2b12b53881
wang-qin928 <2830862261@qq.com>
2026-07-07 18:11:44 +0800

    feat N叉树的前序遍历

---
+++ 树/N叉树的前序遍历.cpp
@@ -0,0 +1,38 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+  public:
+    vector<int> ans;
+    vector<int> preorder(Node *root) {
+        bfs(root);
+        return ans;
+    }
+    void bfs(Node *root) {
+        if (!root)
+            return;
+        ans.push_back(root->val);
+        for (auto node : root->children) {
+            bfs(node);
+        }
+    }
+};
+
+// link : https://leetcode.cn/problems/n-ary-tree-preorder-traversal/
