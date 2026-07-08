14cadc32b2757d891f21be3e3de9aefba37c87a5
wang-qin928 <2830862261@qq.com>
2026-07-08 12:55:05 +0800

    feat 二叉树的前序遍历 非递归做法：栈模拟递归

--- 树/N叉树的前序遍历.cpp
+++ 树/N叉树的前序遍历.cpp
@@ -35,4 +35,27 @@ class Solution {
     }
 };
 
+// 非递归做法
+class Solution {
+public:
+    vector<int> ans;
+    vector<int> preorder(Node* root) {
+        stack<Node*> stk;
+        stk.push(root);
+
+        while (!stk.empty()) {
+            Node* tmp = stk.top();
+            stk.pop();
+            ans.push_back(tmp->val);
+            // 用反向迭代器
+            auto rit = tmp->children.rbegin();
+            while (rit != tmp->children.rend()) {
+                stk.push(*rit);
+                rit++;
+            }
+        }
+        return ans;
+    }
+};
+
 // link : https://leetcode.cn/problems/n-ary-tree-preorder-traversal/
