0c35f6e602265dbbe01b3c887beea85babc35c08
wang-qin928 <2830862261@qq.com>
2026-08-06 14:17:45 +0800

    二叉树的最大路径和 dfs 对于每个节点收集左右子树的单边最大路径 之后更新当前树的答案

--- LeetCode_Hot_100/二叉树/二叉树中的最大路径和.cpp
+++ LeetCode_Hot_100/二叉树/二叉树中的最大路径和.cpp
@@ -11,16 +11,16 @@
  * };
  */
 class Solution {
-public:
+  public:
     int ans = INT_MIN;
-    int maxPathSum(TreeNode* root) {
+    int maxPathSum(TreeNode *root) {
         dfs(root);
         return ans;
     }
 
     // 求经过当前节点的最大路径保存到ans里面
     // 返回值为经过该节点的单边最大路径
-    int dfs(TreeNode* root) {
+    int dfs(TreeNode *root) {
         if (!root)
             return 0;
         // 如果单边路径小于0 那就不要
@@ -46,14 +46,14 @@ public:
  * };
  */
 class Solution {
-public:
+  public:
     int ans = INT_MIN;
-    int maxPathSum(TreeNode* root) {
+    int maxPathSum(TreeNode *root) {
         dfs(root);
         return ans;
     }
     // 返回当前节点单边最大路径
-    int dfs(TreeNode* cur) {
+    int dfs(TreeNode *cur) {
         if (!cur)
             return 0;
         // 舍弃单边路径小于零的部分
@@ -66,4 +66,36 @@ public:
     }
 };
 
-// link : https://leetcode.cn/problems/binary-tree-maximum-path-sum/description/?envType=study-plan-v2&envId=top-100-liked
+// 第二遍写：
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+  public:
+    int ans = INT_MIN;
+    int maxPathSum(TreeNode *root) {
+        dfs(root);
+        return ans;
+    }
+    int dfs(TreeNode *root) {
+        if (!root)
+            return 0;
+        int left = max(0, dfs(root->left));
+        int right = max(0, dfs(root->right));
+
+        ans = max(ans, left + right + root->val);
+        return root->val + max(left, right);
+    }
+};
+
+// link :
+// https://leetcode.cn/problems/binary-tree-maximum-path-sum/description/?envType=study-plan-v2&envId=top-100-liked
