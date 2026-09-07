5e3af2e6ccfea51e3b20ad891769867d84f1947c
wang-qin928 <2830862261@qq.com>
2026-09-07 14:25:49 +0800

    416. 分割等和子集：01背包

--- 秋招每日算法/算法9.6/main.cpp
+++ 秋招每日算法/算法9.6/main.cpp
@@ -1,9 +1,9 @@
-#include <iostream>
 #include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 // 64. 最小路径和 一维空间优化
-int minPathSum(vector<vector<int> > &grid) {
+int minPathSum(vector<vector<int>> &grid) {
     int n = grid[0].size(), m = grid.size();
     // 走到当前格子的最小权值
     vector<int> dp(n + 1, 0);
@@ -19,6 +19,35 @@ int minPathSum(vector<vector<int> > &grid) {
     }
 
     return dp[n];
+}
+
+// 416. 分割等和子集
+bool canPartition(vector<int> &nums) {
+    int sum = 0, max_num = 0, n = nums.size();
+    for (auto e : nums) {
+        sum += e;
+        max_num = max(e, max_num);
+    }
+    if (sum % 2 == 1)
+        return false;
+    if (max_num > sum / 2)
+        return false;
+    int target = sum / 2;
+    // 在数组的前i个数中选择一些数,是否存在一种方案,使得之和恰好等于j
+    vector<vector<bool>> dp(n, vector<bool>(target + 1, false));
+    for (int i = 0; i < n; i++)
+        dp[i][0] = true;
+    dp[0][nums[0]] = true;
+
+    for (int i = 1; i < n; i++) {
+        for (int j = 1; j <= target; j++) {
+            if (j >= nums[i])
+                dp[i][j] = dp[i - 1][j] | dp[i - 1][j - nums[i]];
+            else
+                dp[i][j] = dp[i - 1][j];
+        }
+    }
+    return dp[n - 1][target];
 }
 
 int main() {
